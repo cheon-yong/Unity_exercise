f0bd41c4abea0e608d9d05a06f27d005241b7e9e
yongjun <yes1312@naver.com>
2021-05-17 18:44:57 +0900

    Vector

--- 개론/MMO_Unity/Assets/Scripts/PlayerController.cs
+++ 개론/MMO_Unity/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// 1. 위치 벡터
+// 2. 방향 벡터
+
+struct MyVector
+{
+    public float x;
+    public float y;
+    public float z;
+
+    public float magnitude { get { return Mathf.Sqrt(x*x + y*y + z*z); } }
+    public MyVector normalized { get { return new MyVector(x / magnitude, y / magnitude, z / magnitude); }}
+
+    public MyVector(float x, float y, float z) { this.x = x; this.y = y; this.z = z; }
+
+    public static MyVector operator +(MyVector a, MyVector b)
+    {
+        return new MyVector(a.x + b.x, a.y + b.y, a.z + b.z);
+    }
+
+    public static MyVector operator -(MyVector a, MyVector b)
+    {
+        return new MyVector(a.x - b.x, a.y - b.y, a.z - b.z);
+    }
+
+    public static MyVector operator *(MyVector a, float d)
+    {
+        return new MyVector(a.x * d, a.y * d, a.z * d);
+    }
+}
+
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
@@ -9,7 +40,17 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        
+        MyVector to = new MyVector(10.0f, 0.0f, 0.0f);
+        MyVector from = new MyVector(5.0f, 0.0f, 0.0f);
+        MyVector dir = to - from; // (-5.0f, 0.0f, 0.0f);
+
+        dir = dir.normalized;
+
+        MyVector newPos = from + dir * _speed;
+
+        // 방향 벡터
+        // 1. 거리 (크기) 5 magnitude
+        // 2. 실제 방향  ->
     }
 
     void Update()
@@ -19,6 +60,10 @@ public class PlayerController : MonoBehaviour
 
         // World -> local
         // InverseTransformDirection
+
+        // transform.position.magnitude;
+        // transform.position.normalized;
+
         if (Input.GetKey(KeyCode.W))
             transform.Translate(Vector3.forward * Time.deltaTime * _speed);
         if (Input.GetKey(KeyCode.S))
