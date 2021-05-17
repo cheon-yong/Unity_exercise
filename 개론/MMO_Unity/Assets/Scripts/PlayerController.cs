0d2f07ebdaf55195048db0fd02d95e4447eb9992
yongjun <yes1312@naver.com>
2021-05-17 18:10:07 +0900

    플레이어 회전 및 이동

--- 개론/MMO_Unity/Assets/Scripts/PlayerController.cs
+++ 개론/MMO_Unity/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    float _speed = 10.0f;
+
     void Start()
     {
         
@@ -11,13 +14,18 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        // Local -> World
+        // TransformDirection
+
+        // World -> local
+        // InverseTransformDirection
         if (Input.GetKey(KeyCode.W))
-            transform.position += new Vector3(0.0f, 0.0f, 1.0f);
+            transform.Translate(Vector3.forward * Time.deltaTime * _speed);
         if (Input.GetKey(KeyCode.S))
-            transform.position -= new Vector3(0.0f, 0.0f, 1.0f);
+            transform.Translate(Vector3.back * Time.deltaTime * _speed);
         if (Input.GetKey(KeyCode.A))
-            transform.position -= new Vector3(1.0f, 0.0f, 0.0f);
+            transform.Translate(Vector3.left * Time.deltaTime * _speed);
         if (Input.GetKey(KeyCode.D))
-            transform.position += new Vector3(1.0f, 0.0f, 0.0f);
+            transform.Translate(Vector3.right * Time.deltaTime * _speed);
     }
 }
