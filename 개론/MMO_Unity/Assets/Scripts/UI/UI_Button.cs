22ad78d93d8f454b91c38bf1f2d1944cc7844081
yongjun <yes1312@naver.com>
2021-06-07 19:38:22 +0900

    UI

--- 개론/MMO_Unity/Assets/Scripts/UI/UI_Button.cs
+++ 개론/MMO_Unity/Assets/Scripts/UI/UI_Button.cs
@@ -4,10 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UI_Button : MonoBehaviour
+public class UI_Button : UI_Base
 {
-    Dictionary<Type, UnityEngine.Object[]> _objects = new Dictionary<Type, UnityEngine.Object[]>();
-
     enum Buttons
     {
         PointButton
@@ -30,35 +28,14 @@ public class UI_Button : MonoBehaviour
         Bind<Text>(typeof(Texts));
         Bind<GameObject>(typeof(GameObjects));
 
-        GeT<Text>((int)Texts.ScoreText).text = "Bind Test";
+        Get<Text>((int)Texts.ScoreText).text = "Bind Test";
     }
 
-    void Bind<T>(Type type) where T : UnityEngine.Object
-    {
-        string[] names = Enum.GetNames(type);
-        UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
-        _objects.Add(typeof(T), objects);
-
-        for (int i = 0; i < names.Length; i++)
-        {
-            objects[i] = Util.FindChild<T>(gameObject, names[i], true);
-        }
-    }
-
-    T GeT<T>(int idx) where T : UnityEngine.Object
-    {
-        UnityEngine.Object[] objects = null;
-        if (_objects.TryGetValue(typeof(T), out objects) == false)
-            return null;
-
-        return objects[idx] as T;
-    }
-
+    
     int _score = 0;
 
     public void OnButtonClicked()
     {
         _score++;
-        //_text.text = $"Á¡¼ö : {_score}";
     }
 }
