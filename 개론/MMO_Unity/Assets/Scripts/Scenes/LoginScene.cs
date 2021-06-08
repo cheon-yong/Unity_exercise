1d50fb6ac4c9d421e5a64c472ffd733d2c988534
yongjun <yes1312@naver.com>
2021-06-08 20:07:33 +0900

    Scene Manager #2

--- 개론/MMO_Unity/Assets/Scripts/Scenes/LoginScene.cs
+++ 개론/MMO_Unity/Assets/Scripts/Scenes/LoginScene.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoginScene : BaseScene
 {
@@ -10,8 +11,17 @@ public class LoginScene : BaseScene
 
         SceneType = Define.Scene.Login;
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Managers.Scene.LoadScene(Define.Scene.Game);
+        }
+    }
+
     public override void Clear()
     {
-
+        Debug.Log("LoginScene Clear!");
     }
 }
