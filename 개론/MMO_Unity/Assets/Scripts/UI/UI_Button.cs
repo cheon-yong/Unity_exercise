689cc94632da6259d643604ed0dee386122e0879
yongjun <yes1312@naver.com>
2021-06-08 15:08:52 +0900

    UI

--- 개론/MMO_Unity/Assets/Scripts/UI/UI_Button.cs
+++ 개론/MMO_Unity/Assets/Scripts/UI/UI_Button.cs
@@ -34,18 +34,19 @@ public class UI_Button : UI_Base
         Bind<GameObject>(typeof(GameObjects));
         Bind<Image>(typeof(Images));
 
-        Get<Text>((int)Texts.ScoreText).text = "Bind Test";
+
+        GetButton((int)Buttons.PointButton).gameObject.AddUIEvent(OnButtonClicked);
 
         GameObject go = GetImage((int)Images.ItemIcon).gameObject;
-        UI_EventHandler evt = go.GetComponent<UI_EventHandler>();
-        evt.OnDragHandler += ((PointerEventData data) => { evt.gameObject.transform.position = data.position; });
+        AddUIEvent(go, (PointerEventData data) => { go.transform.position = data.position; }, Define.UIEvent.Drag); 
     }
 
     
     int _score = 0;
 
-    public void OnButtonClicked()
+    public void OnButtonClicked(PointerEventData data)
     {
         _score++;
+        GetText((int)Texts.ScoreText).text = $"Á¡¼ö : {_score}";
     }
 }
