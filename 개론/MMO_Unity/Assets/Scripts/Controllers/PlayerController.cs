7623996f2f0c76e6cebae03fa9ec03d9c601989d
yongjun <yes1312@naver.com>
2021-06-08 18:30:58 +0900

    Inventory #1

--- 개론/MMO_Unity/Assets/Scripts/Controllers/PlayerController.cs
+++ 개론/MMO_Unity/Assets/Scripts/Controllers/PlayerController.cs
@@ -16,10 +16,6 @@ public class PlayerController : MonoBehaviour
     {
         Managers.Input.MouseAction -= OnMouseClicked;
         Managers.Input.MouseAction += OnMouseClicked;
-
-        // TEMP
-
-        UI_Button ui = Managers.UI.ShowPopupUI<UI_Button>();
     }
     public enum PlayerState
     {
