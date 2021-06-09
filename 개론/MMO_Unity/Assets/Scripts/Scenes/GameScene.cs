9c92328c5547cbe7b8cd370869e9705567acae57
yongjun <yes1312@naver.com>
2021-06-09 14:52:05 +0900

    Data Manager #1

--- 개론/MMO_Unity/Assets/Scripts/Scenes/GameScene.cs
+++ 개론/MMO_Unity/Assets/Scripts/Scenes/GameScene.cs
@@ -13,28 +13,6 @@ public class GameScene : BaseScene
         SceneType = Define.Scene.Game;
 
         Managers.UI.ShowSceneUI<UI_Inven>();
-
-        co = StartCoroutine("ExplodeAfterSeconds", 4.0f);
-        StartCoroutine("CoStopExplode", 2.0f);
-    }
-
-    IEnumerator CoStopExplode (float seconds)
-    {
-        Debug.Log("Stop Enter");
-        yield return new WaitForSeconds(seconds);
-        Debug.Log("Stop Execute !!!");
-        if (co != null)
-        {
-            StopCoroutine(co);
-            co = null;
-        }
-    }
-
-    IEnumerator ExplodeAfterSeconds(float seconds)
-    {
-        Debug.Log("Explods Enter");
-        yield return new WaitForSeconds(seconds);
-        Debug.Log("Explods Excute !!!");
     }
 
     public override void Clear()
