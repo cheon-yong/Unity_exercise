ef9eb52b2258dccdbd2716612a1d33f1528209be
yongjun <yes1312@naver.com>
2021-06-10 11:55:48 +0900

    Making Rpg

--- 개론/MMO_Unity/Assets/Scripts/Controllers/CameraController.cs
+++ 개론/MMO_Unity/Assets/Scripts/Controllers/CameraController.cs
@@ -23,7 +23,7 @@ public class CameraController : MonoBehaviour
         if (_mode == Define.CameraMode.QuarterView)
         {
             RaycastHit hit;
-            if (Physics.Raycast(_player.transform.position, _delta, out hit, LayerMask.GetMask("Wall")))
+            if (Physics.Raycast(_player.transform.position, _delta, out hit, _delta.magnitude, LayerMask.GetMask("Wall")))
             {
                 float dist = (hit.point - _player.transform.position).magnitude * 0.8f;
                 transform.position = _player.transform.position + _delta.normalized * dist;
